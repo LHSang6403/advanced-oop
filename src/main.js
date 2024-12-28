08ba723d949e56a6c22644b40415668f44d4d939
HuyBean <nqhuy0711@gmail.com>
2024-12-28 16:36:07 +0700

    Huy - Adjust zip

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ const DOMDisplayStrategy = require('./Display/DOMDisplayStrategy').DOMDisplayStr
 const ConsoleSubscriber = require('./Observer/ConsoleSubscriber').ConsoleSubscriber;
 const DOMSubscriber = require('./Observer/DOMSubscriber').DOMSubscriber;
 const ValidationObserver = require('./Observer/ValidationObserver').ValidationObserver;
+const isStringValidationRule = require('./Rule/ValidationRule/isStringValidationRule').IsStringValidation;
+const CustomValidationRule = require('./Rule/ValidationRule/CustomValidationRule').CustomValidationRule;
 
 const framework = ValidationFramework.getInstance();
 
@@ -24,8 +26,15 @@ const passwordValidation = new ValidationRuleBuilder()
   .addRegex(/\d/, "Password must have at least one digit")
   .build();
 
+const isStringValidation = new isStringValidationRule();
+
+const customValidation = new CustomValidationRule((value) => value === "hello", "Value must be 'hello'");
+
+
 framework.addRule('username', usernameValidation);
 framework.addRule('password', passwordValidation);
+framework.addRule('isString', isStringValidation);
+framework.addRule('custom', customValidation);
 
 const observer = new ValidationObserver();
 observer.subscribe(new ConsoleSubscriber());
@@ -38,6 +47,8 @@ framework.setDisplayStrategy({
 const data = {
   username: "", // Không hợp lệ
   password: "", // Không hợp lệ
+  isString: 123, // Không hợp lệ
+  custom: "hello1" 
 };
 
 const result = framework.validate(data);
